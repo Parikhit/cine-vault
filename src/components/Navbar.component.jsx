07edfcc2ea0890057942415e3ec510431f22934e
Parikhit <parikhitritzz@gmail.com>
2024-04-30 00:07:16 +0530

    resolving issues

--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -1,16 +1,21 @@
 import NavbarItem from './NavbarItem.component';
 
+import { Suspense } from 'react';
+import Loading from '@/app/loading';
+
 const Navbar = () => {
     return (
         <div className='flex justify-center gap-20 dark:bg-gray-500 bg-amber-200 p-4 lg:text-lg'>
-            <NavbarItem
-                title='Top Rated Movies'
-                param='movies'
-            />
-            <NavbarItem
-                title='Top Rated Series'
-                param='series'
-            />
+            <Suspense fallback={<Loading />}>
+                <NavbarItem
+                    title='Top Rated Movies'
+                    param='movies'
+                />
+                <NavbarItem
+                    title='Top Rated Series'
+                    param='series'
+                />
+            </Suspense>
         </div>
     );
 };
--- src/components/NavbarItem.component.jsx
+++ src/components/NavbarItem.component.jsx
@@ -2,27 +2,23 @@
 
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
-import { Suspense } from 'react';
-import Loading from '@/app/loading';
 
 const NavbarItem = ({ title, param }) => {
     const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
     return (
-        <Suspense fallback={<Loading />}>
-            <div>
-                <Link
-                    className={`hover:text-amber-500 font-semibold ${
-                        genre === param
-                            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-                            : ''
-                    }`}
-                    href={`/?genre=${param}`}
-                >
-                    {title}
-                </Link>
-            </div>
-        </Suspense>
+        <div>
+            <Link
+                className={`hover:text-amber-500 font-semibold ${
+                    genre === param
+                        ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+                        : ''
+                }`}
+                href={`/?genre=${param}`}
+            >
+                {title}
+            </Link>
+        </div>
     );
 };
 
