96ca0df8f603abdbc788053c85bebadc89da4d11
Parikhit <parikhitritzz@gmail.com>
2024-05-02 19:39:07 +0530

    Resolving issues

--- src/app/utils/requests.js
+++ src/app/utils/requests.js
@@ -1,23 +1,21 @@
+'use server';
+
 const httpGetAllMovies = async () => {
-    'use server';
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies`);
     return await response.json();
 };
 
 const httpGetAllSeries = async () => {
-    'use server';
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/series`);
     return await response.json();
 };
 
 const httpGetSingleMovie = async (id) => {
-    'use server';
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies/${id}`);
     return await response.json();
 };
 
 const httpGetSingleSeries = async (id) => {
-    'use server';
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/series/${id}`);
     return await response.json();
 };
