615ba5d64f2a9c3f18a5b06c0ec76614743f21fc
Parikhit <parikhitritzz@gmail.com>
2024-05-02 19:16:57 +0530

    Resolving issues

--- src/app/utils/requests.js
+++ src/app/utils/requests.js
@@ -1,22 +1,20 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
 const httpGetAllMovies = async () => {
-    const response = await fetch(`${API_URL}/movies`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies`);
     return await response.json();
 };
 
 const httpGetAllSeries = async () => {
-    const response = await fetch(`${API_URL}/series`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/series`);
     return await response.json();
 };
 
 const httpGetSingleMovie = async (id) => {
-    const response = await fetch(`${API_URL}/movies/${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies/${id}`);
     return await response.json();
 };
 
 const httpGetSingleSeries = async (id) => {
-    const response = await fetch(`${API_URL}/series/${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/series/${id}`);
     return await response.json();
 };
 
