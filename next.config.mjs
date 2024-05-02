92c7d4cfe940fb7090bf7b1619472d3c8ce76425
Parikhit <parikhitritzz@gmail.com>
2024-05-02 18:47:10 +0530

    Resolving issues

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
             },
         ],
     },
+
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/:path*', // Proxy to Backend
+            },
+        ];
+    },
 };
 
 export default nextConfig;
